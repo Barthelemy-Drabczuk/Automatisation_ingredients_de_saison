82b88f6c305bbcf60135bb699d44937035046482
barthelemy-drabczuk <percevase@gmail.com>
2023-05-20 15:55:51 +0200

    parser completed for current month

--- parse.py
+++ parse.py
@@ -8,5 +8,11 @@ class Parser:
     def __init__(self, html_content) -> None:
         self.__bs_obj = BeautifulSoup(html_content.lower(), 'html.parser')
     
-    def get_current_month(self) -> str:
-        return self.__bs_obj.find_all(class_="month")[datetime.now().month - 1]
+    def get_current_month(self) -> dict:
+        raw = self.__bs_obj.find_all(class_="month")[datetime.now().month - 1]
+        # print(raw.prettify())
+        items = [[i.text.rstrip().lstrip() for i in b.find_all('li')] for b in raw.find_all("ul")]
+        return {
+            items[0][0] : items[1],
+            items[0][1] : items[2]
+        }
